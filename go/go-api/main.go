4798d60a465a3aa62db55486b39dc7286a76516d
awyssa <50834877+Awyssa@users.noreply.github.com>
2024-09-25 21:21:16 +0100

    I still do not understand Mutex at all...

--- go/go-api/main.go
+++ go/go-api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"sync"
@@ -25,6 +26,20 @@ func registerHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
+
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	mu.Lock()
+	users[user.Email] = user
+	mu.Unlock()
+
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprintf(w, "User registered successfully")
 }
 
 func main() {
