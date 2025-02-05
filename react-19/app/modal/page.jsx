b59a0b59e65ee72a3eac3e96106a749168a02d40
awyssa <50834877+Awyssa@users.noreply.github.com>
2025-02-05 22:58:55 +0000

    add react19 things with portal

---
+++ react-19/app/modal/page.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
+
+const Modal = ({ children }) => {
+
+  const elRef = useRef(null);
+
+  if(!elRef.current) { 
+    // Maintain the same div per each render
+    elRef.current = document.createElement("div")
+  }
+
+  useEffect(() => {
+    const modalRoot = document.getElementById("modal")
+    modalRoot?.appendChild(elRef.current)
+
+    return "TODO"
+  }, [])
+
+  return createPortal(<div>{children}</div>, elRef.current)
+}
+
+export default Modal;
