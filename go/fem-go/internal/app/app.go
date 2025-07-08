2f0ab4835c16ec8467fea3c63dd8e17a86d8e20f
Michael <michael.ehson@gmail.com>
2025-07-08 23:41:40 +0100

    more go api stuff

--- go/fem-go/internal/app/app.go
+++ go/fem-go/internal/app/app.go
@@ -5,17 +5,26 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"project-1/internal/api"
 )
 
 type Application struct {
 	Logger *log.Logger
+	WorkoutHandler *api.WorkoutHandler
 }
 
 func NewApplication() (*Application, error) {
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
+	// Our stores will go here
+
+	// Our handlers will go here
+	workoutHandler := api.NewWorkoutHandler()
+
 	app := &Application{
 		Logger: logger,
+		WorkoutHandler: workoutHandler,
 	}
 
 	return app, nil
