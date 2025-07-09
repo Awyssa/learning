8712fb09b3337b9cadb99c6a5d37672296fa1962
Michael <michael.ehson@gmail.com>
2025-07-09 02:20:07 +0100

    docker, database, postgres, goose additions

--- go/fem-go/internal/app/app.go
+++ go/fem-go/internal/app/app.go
@@ -1,30 +1,36 @@
 package app
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"project-1/internal/api"
+	"project-1/internal/store"
 )
 
 type Application struct {
-	Logger *log.Logger
-	WorkoutHandler *api.WorkoutHandler
+	Logger 					*log.Logger
+	WorkoutHandler 	*api.WorkoutHandler
+	DB 							*sql.DB
 }
 
 func NewApplication() (*Application, error) {
+	pgDB, err := store.Open()
+	if(err != nil) {
+		return nil, err
+	}
+
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
-	// Our stores will go here
-
-	// Our handlers will go here
 	workoutHandler := api.NewWorkoutHandler()
 
 	app := &Application{
 		Logger: logger,
 		WorkoutHandler: workoutHandler,
+		DB: pgDB,
 	}
 
 	return app, nil
